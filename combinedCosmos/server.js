ec8d5bc0081f6e95f9a83e207de8fa7291701abc
seohokim <shk9946@gmail.com>
2023-05-28 22:53:35 +0900

    Cosmos Visualization

--- combinedCosmos/server.js
+++ combinedCosmos/server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const hostname = '127.0.0.1';
-const port = 3002;
+const port = 4000;
 
 const server = http.createServer((req, res) => {
   let filePath;
